a304e0483952131ab4cfa2faec31f9ece88d3546
Anand Ahire <anandsahire8595@gmail.com>
2026-06-28 21:19:35 +0530

    Update Navbar component

--- InterviewAgent/client/src/components/Navbar.jsx
+++ InterviewAgent/client/src/components/Navbar.jsx
@@ -4,9 +4,13 @@ import { motion } from "motion/react"
 import { BsRobot, BsCoin } from "react-icons/bs";
 import { HiOutlineLogout } from "react-icons/hi";
 import { FaUserAstronaut } from "react-icons/fa";
-
+import { useState } from 'react';
+import { useNavigate } from 'react-router';
 function Navbar() {
   const {userData} = useSelector((state)=>state.user)
+  const [showCreditPopup,setShowCreditPopup] = useState(false)
+  const [showUserPopup,setShowUserPopup] = useState(false)
+  const navigate = useNavigate()
   
   return (
     <div className='bg-[#f3f3f3] flex justify-center px-4 pt-6'>
@@ -14,7 +18,7 @@ function Navbar() {
         initial={{opacity:0, y:-40}}
         animate={{opacity:1, y:0}}
         transition={{duration: 0.3}}
-        className='w-full max-w-6xl bg-white rounded-[24px] shadow-sm border border-gray-200 px-8 py-4 flex justify-between items-center relative'
+        className='w-full max-w-6xl bg-white rounded-3xl shadow-sm border border-gray-200 px-8 py-4 flex justify-between items-center relative'
       >
         <div className='flex items-center gap-3 cursor-pointer'>
           <div className='bg-black text-white p-2 rounded-lg'>
@@ -25,10 +29,20 @@ function Navbar() {
 
         <div className='flex items-center gap-6 relative'>
             <div className='relative'>
-            <button className='flex items-center gap-2 bg-gray-100 px-4 py-2 rounded-full text-md hover:bg-gray-200 transition'>
+            <button onClick={()=>setShowCreditPopup
+            (!showCreditPopup)} className='flex items-center gap-2 bg-gray-100 px-4 py-2 rounded-full text-md hover:bg-gray-200 transition'>
               <BsCoin size={20}/>
               {userData?.credits || 0}
             </button>
+            {showCreditPopup && (
+              <div className='absolute right-[-50px] mt-3 w-64 bg-white shadow-xl border border-gray-200 rounded p-5 z-50'>
+                <p className='text-sm text-gray-600 mb-4'>Need more credits to continue interviews?</p>
+                <button onClick={()=>navigate("/pricing")} className='w-full bg-black 
+                text-white py-2 rounded-lg text-sm'>
+                  Buy more credits</button>
+
+              </div>
+            )}
           </div>
 
           <div className='relative'>
